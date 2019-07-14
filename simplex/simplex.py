29652d60666e8b12b7a0e98182675fb1d96d4365
Christopher Wetherill <chris@tbmh.org>
2019-07-14 14:49:32 -0400

    Added new routes for data/model management
    Summary: This adds new routes for data and model management. Just stubbing out the HTML files currently, nothing crazy
    
    Test Plan: Ran locally
    
    Reviewers: brooke
    
    Reviewed By: brooke
    
    Differential Revision: https://phabricator.tbmh.org/D5

--- simplex/simplex.py
+++ simplex/simplex.py
@@ -15,19 +15,34 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 def index():
     return render_template('index.html')
 
+
+@app.route('/manage/data', methods=['GET', 'POST'])
+def manage_data():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+
+    return render_template('manage/data.html')
+
+
+@app.route('/manage/models', methods=['GET', 'POST'])
+def manage_models():
+    if request.method == 'POST':
+        return redirect(url_for('index'))
+
+    return render_template('manage/models.html')
+
+
 @app.route('/model_performance', methods=['GET', 'POST'])
 def model_performance():
     if request.method == 'POST':
-        
         return redirect(url_for('index'))
 
-    return render_template('model_performance.html')
+    return render_template('compare/model_performance.html')
 
 
 @app.route('/feature_distribution', methods=['GET', 'POST'])
 def feature_distribution():
     if request.method == 'POST':
-        
         return redirect(url_for('index'))
 
-    return render_template('feature_distribution.html')
+    return render_template('compare/feature_distribution.html')
